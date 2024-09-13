fa6da0c5e0326a86c49ec497a254357966f59102
Kingbegaa <barakachachabegaa@gmail.com>
2024-09-13 18:03:47 +0300

    Update ping.js

--- bega/ping.js
+++ bega/ping.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
-zokou({ nomCom: "test", reaction: "⚡️", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "test", reaction: "🙂‍↔️", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '*🌍𝑩𝒐𝒕 𝒊𝒔 𝒐𝒏𝒍𝒊𝒏𝒆🌍* 🙏 \n\n ' + "𝑻𝒉𝒆 𝒃𝒐𝒕 𝒊𝒔 𝒄𝒖𝒓𝒓𝒆𝒏𝒕𝒍𝒚 𝒘𝒐𝒓𝒌𝒊𝒏𝒈 𝒐𝒏 𝒂 𝒈𝒐𝒐𝒅 𝒔𝒑𝒆𝒆𝒅😉👍";
-    let d = '                                                                           𝑯𝒆𝒂𝒍𝒕𝒉 𝒔𝒕𝒂𝒕𝒖𝒔✨';
+    let z = '*𝙱𝙾𝚃 𝙸𝚂 𝙾𝙽𝙻𝙸𝙽𝙴*  \n\n ' + "𝙱𝙰𝚁𝙰𝙺𝙰-𝙼𝙳 𝙸𝚂 𝙸𝚂 𝙲𝚄𝚁𝚁𝙴𝙽𝚃𝙻𝚈 𝚆𝙾𝚁𝙺𝙸𝙽𝙽𝙶 𝙶𝙾𝙾𝙳 𝙾𝙽 𝙰 𝚂𝙿𝙴𝙴𝙳.🙂‍↔️";
+    let d = '                                                                           𝙷𝙴𝙰𝙻𝚃𝙷 𝚂𝚃𝙰𝚁𝚄𝚂✨';
     let varmess = z + d;
     var mp4 = 'https://telegra.ph/file/ce58cf8c538b1496fda33.mp4';
     await zk.sendMessage(dest, { video: { url: mp4 }, caption: varmess });
