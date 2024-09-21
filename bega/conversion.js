814465ed23757a073b48268c4f0d4a5074e1ce65
Baraka Bega <barakachachabega@gmail.com>
2024-09-21 15:32:05 +0300

    Update conversion.js

--- bega/conversion.js
+++ bega/conversion.js
@@ -376,3 +376,20 @@ zokou({ nomCom: "url1", categorie: "Conversion", reaction: "👨🏿‍💻" }, 
       repondre('Opps error');
   }
 });
+
+
+const { bot, getUrl } = require('../framework/zokou)
+
+zokou(
+  {
+    pattern: 'url ?(.*)',
+    desc: 'Image/Video to url',
+    type: 'misc',
+  },
+  async (message, match) => {
+    if (!message.reply_message || (!message.reply_message.image && !message.reply_message.video))
+      return await message.send('*Reply to a image | video*\nurl imgur - for imgur url')
+    await message.send(
+      await getUrl(await message.reply_message.downloadAndSaveMediaMessage('url2'), match)
+    )
+  });
